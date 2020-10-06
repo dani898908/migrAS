11fd061ce27dea19135692dd0be9c508be447e1b
daniela <danielabonillac@hotmail.com>
2020-10-06 19:55:37 +0200

    Updates

---
+++ precache-manifest.137b62988181e4ab89a8e0619c3b9dbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3e24683081dac0aaf0e324a774af66",
+    "url": "/migrAS/index.html"
+  },
+  {
+    "revision": "349af2b69922ef15d2ec",
+    "url": "/migrAS/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1531872ba05c76c36b59",
+    "url": "/migrAS/static/css/main.c9991d7f.chunk.css"
+  },
+  {
+    "revision": "349af2b69922ef15d2ec",
+    "url": "/migrAS/static/js/2.e56eb618.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/migrAS/static/js/2.e56eb618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1531872ba05c76c36b59",
+    "url": "/migrAS/static/js/main.3929097a.chunk.js"
+  },
+  {
+    "revision": "d8a97662db097dfb7b8c",
+    "url": "/migrAS/static/js/runtime-main.51dde0c7.js"
+  },
+  {
+    "revision": "add73c0754695760c6162993aefef4d0",
+    "url": "/migrAS/static/media/mario.add73c07.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/migrAS/precache-manifest.137b62988181e4ab89a8e0619c3b9dbb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/migrAS/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
