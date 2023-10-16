902703f9c3062caad63ed64634ea5e6b7a8db48d
DANIELA BONILLA <60890987+dani898908@users.noreply.github.com>
2023-10-16 13:01:11 -0500

    modifyend style

--- src/Nuevo.js
+++ src/Nuevo.js
@@ -20,13 +20,9 @@ export default function Pop() {
        muni.geometry.coordinates[0]
      ]}
 
-
       onClick={() => {
-        setActiveMuni(muni);
-
-      }}
-
-     />
+      setActiveMuni(muni);
+      }} />
 
      )}
      {activeMuni && (
